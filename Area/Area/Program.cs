1a8ecc6fa855e22ca392cb406d3b0909f5332a01
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:11:47 -0300

    Uri 1012 Area

---
+++ Area/Area/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            TRIANGULO = A * C / 2.0;
+            CIRCULO = 3.14159 * C * C;
+            TRAPEZIO = (A + B) / 2.0 * C;
+            QUADRADO = B * B;
+            RETANGULO = A * B;
+
+            Console.WriteLine("TRIANGULO: " + TRIANGULO.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + CIRCULO.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + TRAPEZIO.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + QUADRADO.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + RETANGULO.ToString("F3", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+        }
+    }
+}
